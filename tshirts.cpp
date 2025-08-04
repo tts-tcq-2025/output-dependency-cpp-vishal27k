183ab9b1e65c5d88e2d9969542b32c13c243564a
vishal27k <52737349+vishal27k@users.noreply.github.com>
2025-08-04 10:08:50 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -17,10 +17,9 @@ void testTshirtSize() {
     assert(size(37) == 'S');
     assert(size(40) == 'M');
     assert(size(43) == 'L');
-    assert(size(38) == 'S');
     assert(size(38) == 'M');
-    assert(size(42) == 'M');
     assert(size(42) == 'L');
     std::cout << "All is well (maybe!)\n";
 }
+
 
