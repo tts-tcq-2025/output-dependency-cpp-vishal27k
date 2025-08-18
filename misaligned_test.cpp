bad40d24cd5e449c69d407b6507d317f23e8c619
vishal27k <52737349+vishal27k@users.noreply.github.com>
2025-08-18 08:42:25 +0530

    Update misaligned_test.cpp

--- misaligned_test.cpp
+++ misaligned_test.cpp
@@ -1,1 +1,16 @@
+#include "misaligned.cpp"
+#include <vector>
+#include <string>
 
+vector<string&> capturedLines;
+void MockOutputFunc(std::string& lineContent){
+    actualManual.push_back(lineContent);
+}
+
+void testPrintColorMap() {
+    std::cout << "\nPrint color map test\n"; 
+    int result = printColorMap(MockOutputFunc); 
+    assert(result == 25);
+    assert(capturedLines[1]="1 | White | Orange");
+    std::cout << "All is well (maybe!)\n";
+}
