dcc513a1b9cc7655b1c8e054d9aa79b05ac5e23e
vishal27k <52737349+vishal27k@users.noreply.github.com>
2025-08-18 08:35:49 +0530

    Create misaligned.hpp

---
+++ misaligned.hpp
@@ -0,0 +1,10 @@
+#ifndef MISALIGNED_HPP
+#define MISALIGNED_HPP
+
+#include <vector>
+#include <string>
+#include <tuple>
+
+int printColorMap(const std::function<void(const std::string&)>& outputFunc);
+
+#endif // COLOR_MAP_H
